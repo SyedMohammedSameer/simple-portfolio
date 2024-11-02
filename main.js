cf1a79d63506437ecdc9caf312ce12401a8b77c6
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-02 20:59:19 +0430

    add new featuers

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
+const menuBtn = document.getElementById("menu-btn");
+const link = document.getElementById("link");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.add("open");
+});
+
+sidebar.addEventListener("click", () => {
+    sidebar.classList.remove("open");
+});
+
 var typed = new Typed(".text", {
     strings: ["Software Engineer", "Machine Learning Engineer", "AI Enthusiast"],
     typeSpeed: 100,
-    backSpeed:100,
+    backSpeed: 100,
     backDelay: 1000,
-    loop: true
-})
+    loop: true,
+});
 
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
+    sections.forEach((sec) => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        let id = sec.getAttribute("id");
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            navLinks.forEach((link) => {
+                link.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
             });
         }
     });
